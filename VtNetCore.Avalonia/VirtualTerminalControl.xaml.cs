dcb950f077b0673bb20c0a1cdae3f84b0b3aaa06
Hendrik Mennen <hendrik.mennen@hs-osnabrueck.de>
2020-05-21 11:42:09 +0200

    use default colors

--- VtNetCore.Avalonia/VirtualTerminalControl.xaml.cs
+++ VtNetCore.Avalonia/VirtualTerminalControl.xaml.cs
@@ -587,6 +587,9 @@ namespace VtNetCore.Avalonia
 
         public IBrush GetSolidColorBrush(string hex)
         {
+            Console.WriteLine(hex);
+            if (hex == "#0C0C0C") return this.Background;
+            else if (hex == "#CCCCCC") return this.Foreground;
             byte a = 255; // (byte)(Convert.ToUInt32(hex.Substring(0, 2), 16));
             byte r = (byte)(Convert.ToUInt32(hex.Substring(1, 2), 16));
             byte g = (byte)(Convert.ToUInt32(hex.Substring(3, 2), 16));
